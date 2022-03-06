3f63b81b686e29496960ce7eff0755639898b69f
Mateusz Koniuszewski <mateusz.koniuszewski@gmail.com>
2022-03-06 16:06:03 +0100

    Template for cyclic_events.py

---
+++ APIStockServer/modules/cyclic_events.py
@@ -0,0 +1,31 @@
+import time
+from timeloop import Timeloop
+from datetime import timedelta
+
+
+class CyclicEvents:
+
+    __ENGINE = Timeloop()
+
+    @classmethod
+    def start(cls):
+        cls.__ENGINE.start(block=True)
+
+    @staticmethod
+    @__ENGINE.job(interval=timedelta(seconds=2))
+    def sample_job_every_2s():
+        print("2s job current time : {}".format(time.ctime()))
+
+    @staticmethod
+    @__ENGINE.job(interval=timedelta(seconds=5))
+    def sample_job_every_5s():
+        print("5s job current time : {}".format(time.ctime()))
+
+    @staticmethod
+    @__ENGINE.job(interval=timedelta(seconds=10))
+    def sample_job_every_10s():
+        print("10s job current time : {}".format(time.ctime()))
+
+
+if __name__ == "__main__":
+    CyclicEvents.start()
