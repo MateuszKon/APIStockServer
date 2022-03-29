513b5fcec571e2bb0f8072ec126ca9b7df7e6c93
Mateusz Koniuszewski <mateusz.koniuszewski@gmail.com>
2022-03-29 19:14:26 +0200

    Scheduler for cyclic alerts checking

---
+++ APIStockServer/alerts/alerts_scheduler.py
@@ -0,0 +1,34 @@
+import datetime
+import time
+
+import schedule
+
+
+class AlertsScheduler:
+
+    def __init__(self, start_hour, stop_hour, funct, *args, **kwargs):
+        self.start_hour = start_hour
+        self.stop_hour = stop_hour
+        self.funct = funct
+        self.args = args
+        self.kwargs = kwargs
+
+        self.schedule_next_day()
+
+    @classmethod
+    def run_waiting_alert(cls):
+        schedule.run_pending()
+
+    def set_hourly_alerts(self):
+        if datetime.datetime.today().weekday() < 5:  # schedule only from Monday to Friday
+            schedule.every().second.until(self.stop_hour).do(self.funct, *self.args, **self.kwargs)
+
+    def schedule_next_day(self):
+        schedule.every().day.at(self.start_hour).do(self.set_hourly_alerts)
+
+
+if __name__ == "__main__":
+    AlertsScheduler("19:08", "21:00", lambda: print("Current time: {}".format(time.time())))
+    while True:
+        AlertsScheduler.run_waiting_alert()
+        time.sleep(1)
